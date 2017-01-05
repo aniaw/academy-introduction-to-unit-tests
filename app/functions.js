6f38c180e000cf6b4c9c6b93056b7b01892d2138
aniaw <ania_w@outlook.com>
2017-01-05 09:52:48 +0100

    exercise

--- app/functions.js
+++ app/functions.js
@@ -9,6 +9,22 @@
                 return 'You cannot divide by 0';
             }
             return a/b;
+        },
+        calculateDiscount: function (age)
+        {
+            if (age >=0 && age < 18){
+                return 30;
+            }
+            else if (age >=18 && age <= 99) {
+                return 10;
+            }
+            else if (age === 100){
+                return 'Entrance free'
+            }
+            else {
+                return 'I think you gave an incorrect age!';
+            }
         }
+
     };
 })();
